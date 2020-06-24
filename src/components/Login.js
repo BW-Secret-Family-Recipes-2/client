367b5f6123b356b44bb86981b62bf865d9b0321c
KamaMichaelis <achantayd@gmail.com>
2020-06-24 12:18:45 -0400

    another error check

--- src/components/Login.js
+++ src/components/Login.js
@@ -143,7 +143,7 @@ export default function Login(props) {
 
 
                 </div><br />
-                
+               
                 <div>
                     {isLoading && <LoadingLottie something={animationData} width={200} height={200} />}
                     <button className='submitBtn'>Submit</button>
